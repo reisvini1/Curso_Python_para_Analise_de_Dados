df77118d2439a82ddf7b825d5d40d67a45b07e97
Vinicius Reis <vinicius.moreira2003@gmail.com>
2020-12-10 15:43:33 -0300

    Exercícios Cap. 02

---
+++ exercícios_cap02/ex02cap02.py
@@ -0,0 +1,5 @@
+# Exercício 2 - Crie uma lista de 5 objetos e imprima na tela
+
+lista = ["arroz", "feijao", "carne", "ovo", "leite"]
+
+print(lista)
---
+++ exercícios_cap02/ex09cap02.py
@@ -0,0 +1,7 @@
+# Exercício 9 - Crie uma lista de 4 elementos. O primeiro elemento deve ser uma string,
+# o segundo uma tupla de 2 elementos, o terceiro um dcionário com 2 chaves e 2 valores e
+# o quarto elemento um valor do tipo float.
+# Imprima a lista na tela.
+
+list = ["string1", (10, 90), {"key1":60, "key2":90}, 8.99]
+print(list)
